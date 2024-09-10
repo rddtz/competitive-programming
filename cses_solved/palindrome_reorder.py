825d0e20dcccb5ec99fb65961f6775252a4992d6
Raddatz! <rayandematos@gmail.com>
2024-09-10 14:32:57 -0300

    cses 1755 - python for strings!

---
+++ cses_solved/palindrome_reorder.py
@@ -0,0 +1,27 @@
+str = input()
+
+letters = {}
+
+for l in str:
+    if l not in letters:
+        letters[l] = 1
+    else:
+        letters[l] += 1
+    
+impares = 0
+new_str = ""
+m = 0
+for k in letters.keys():
+    if(letters[k] % 2):
+        new_str = new_str[:m] + letters[k]*k + new_str[m:]
+        m += 1
+        impares += 1
+    else:
+        t = int(letters[k]/2)
+        new_str = k*t + new_str + k*t
+        m += t
+            
+if(impares > 1):
+    print("NO SOLUTION")
+else:
+    print(new_str)
