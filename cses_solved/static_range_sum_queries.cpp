e65f369fe5fa916fdacdccbad8edf38036197ab5
Raddatz! <rayandematos@gmail.com>
2024-09-11 09:10:19 -0300

    cses 1646

---
+++ cses_solved/static_range_sum_queries.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#define ll long long int
+
+using namespace std;
+
+int main(){
+
+  ios::sync_with_stdio(false);
+
+  int n, q;
+  int a, b;
+  cin >> n;
+  cin >> q;
+
+  ll sums[n+1] = {0};
+
+  ll aux;
+  ll sum = 0;
+  for(int i = 1; i <= n; i++){
+    cin >> aux;
+    sum += aux;
+    sums[i] = sum;
+  }
+
+  for(int i = 0; i < q; i++){
+
+    cin >> a >> b;
+    cout << sums[b] - sums[a-1] << '\n';
+    
+  }
+  
+  return 0;
+}
