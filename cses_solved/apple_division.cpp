f56bbd7dbf77c77fd67bac52a053ca9b7e1faee1
Rayan Raddatz <rayandematos@gmail.com>
2024-09-12 20:02:59 -0300

    cses 1623 - hard one so i get the solution and studied it

---
+++ cses_solved/apple_division.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+#define ll long long
+
+using namespace std;
+
+ll l[20] = {};
+
+ll fm(int index, int size, ll a, ll b){
+
+  if(index >= size){
+      if(a >= b){
+      return a - b;
+    }
+    return b - a;
+  }
+
+  ll ga = fm(index + 1, size, a + l[index], b);
+  ll gb = fm(index + 1, size, a, b + l[index]);
+  
+  return min(ga, gb);
+  
+}
+
+int main(){
+
+  ios::sync_with_stdio(false);
+
+  int n;
+  cin >> n;
+  
+  for(int i = 0; i < n; i++){
+    cin >> l[i];
+  }
+
+  cout << fm(0, n, 0, 0) << '\n';
+  
+  return 0;
+}
